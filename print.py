c29adc51562b0f7b44608f3e2d63394108530b72
16808910041680 <forforfirefire@gmail.com>
2025-03-23 10:58:09 +1100

    iou

---
+++ print.py
@@ -0,0 +1,9 @@
+print ("Gimme 4 Random Numbers; 1-129749656707530815886285663790")
+num1= int (input("Num 1:"))
+num2= int (input("Num 2:"))
+num3= int (input("Num 3:"))
+num4= int (input("Num 4:"))
+print ("Thanks!")
+sum = num1+num2+num3+num4
+percent = (sum/400)*129749656707530815886285663790
+print ("You just got: ", percent)
