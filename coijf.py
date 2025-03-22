58c0d959dcb1afb5d58b6d3a667316e9c3179d9e
16808910041680 <forforfirefire@gmail.com>
2025-03-23 10:43:35 +1100

    hi

---
+++ coijf.py
@@ -0,0 +1,12 @@
+a=149
+b=23738
+c=4376
+d=878356
+e=6538
+f=35629
+g=29536
+print (a,b,c,d,e)
+sum = a+b+c+d+e
+print ("The sum of", a,",", b,",", c,",", d,"and", e," is", sum)
+avg = sum/7
+print ("The average of", a,",", b,",", c,",", d,"and", e," is", avg)
